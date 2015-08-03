0628a8e795eba56fd383bb63dd6e54a9e98b3163
jackcao <jack.cao@163.com>
2015-08-03 13:33:51 +0800

    s

--- html/myjs/changepage.js
+++ html/myjs/changepage.js
@@ -21,33 +21,74 @@ var idHtmlMap = {
 	"workMain" : "workMain.html",
 	"entertainmentMain" : "entertainmentMain.html",
 	"foodDetail" : "foodDetail.html",
-	"workMain" : "workMain.html",
-	"entertainmentMain" : "entertainmentMain.html"
+	"changeCity" : "changeCity.html",
+	"message" : "message.html",
+	"friendsMsg":"friendsMsg.html",
+	"servNoticeMsg":"servNoticeMsg.html",
+	"servRemindMsg":"servRemindMsg.html",
+	"subscribeMsg":"subscribeMsg.html",
+	"systemMsg":"systemMsg.html"
 };
 
 /* 显示子页面，隐藏原页面 */
 function changeSubPage(newpageid, oldpageid,params) {
+	if (parent.$('#indexPage').length>0){
+		parent.$('#footNavBar').css('display','none');
+		//刷新parent.displayFrameId的高度
+		var newh =parent.window.innerHeight
+		|| parent.document.documentElement.clientHeight
+		|| parent.document.body.clientHeight;
+		parent.$('#'+parent.displayFrameId).css('height',newh+'px');
+		
+	}
 	//如果没有otherpage iframe,则在body里创建一个iframe
 	if ($('#otherPage').length==0){
-		$('<iframe src="#" style="display: none;width:100%;height:100%;" id="otherPage" />').appendTo('body'); 
-	}
-	var oldsrc = $('#otherPage').attr('src');
-	var newsrc = idHtmlMap[newpageid];
-	if (oldsrc != newsrc) {
+		$('<iframe src="#" style="display: none;width:100%;height:100%;" id="otherPage"/>').appendTo('body');
+		var newsrc = idHtmlMap[newpageid];
 		if (params!=null){
 			newsrc = newsrc + '?' + params;
 		}
 		$('#otherPage').attr('src', newsrc);
+	}else{
+		var oldsrc = $('#otherPage').attr('src');	
+		var newsrc = idHtmlMap[newpageid];
+		if (oldsrc != newsrc) {
+			if (params!=null){
+				newsrc = newsrc + '?' + params;
+			}
+			$('#otherPage').remove();
+			$('<iframe src="#" style="display: none;width:100%;height:100%;" id="otherPage"/>').appendTo('body');
+			$('#otherPage').attr('src', newsrc);
+		}
 	}
+	
 	$('#otherPage').css('display', 'block');
 	$('#' + oldpageid).css('display', 'none');
 }
 
 /* 关闭子页面 */
 function closeSubPage(destpageid) {
+	//显示主页面上的导航栏，并重新设置当前iframe的高度
+	if (parent.parent.$('#indexPage').length>0){
+		parent.parent.$('#footNavBar').css('display','block');
+		
+		var newh =parent.window.innerHeight
+		|| parent.document.documentElement.clientHeight
+		|| parent.document.body.clientHeight;
+		var navbarh = parent.parent.$('#footNavBar').height();
+		
+		parent.parent.$('#'+parent.parent.displayFrameId).css('height',newh-navbarh +'px');
+	}
 	parent.$('#otherPage').css('display', 'none');
 	if (destpageid == null) {
 		destpageid = parent.$('[data-role="page"]').attr('id');
 	}
 	parent.$('#' + destpageid).css('display', 'block');
 }
+
+/*$(document).bind('swipeleft',function(){
+	alert('swipeleft');
+})
+$(document).bind('swiperight',function(){
+	alert('swiperight');
+})*/
